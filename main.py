d96c230f8089a701199165d1c491fc5bab6d3372
Z_zcyper <128421427+Zzcyper@users.noreply.github.com>
2023-07-16 20:19:42 +0300

    main.py

--- main.py
+++ main.py
@@ -11,14 +11,12 @@ def track_ip():
     except requests.RequestException:
         messagebox.showerror("Error", "An error occurred while retrieving the IP address.")
 
-# Create a tkinter window
+
 window = tk.Tk()
 window.title("IP Tracker")
-window.configure(bg="gray")  # Set the background color to gray
+window.configure(bg="gray")  
 
-# Create track button
 track_button = tk.Button(window, text="Track IP", command=track_ip)
 track_button.pack(pady=10)
 
-# Run the tkinter event loop
 window.mainloop()
